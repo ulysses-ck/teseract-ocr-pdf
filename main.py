70b0389d5eecfb32a00304be18b3033693ad60a6
Ulises688apaza <ulisesapaza.280@gmail.com>
2021-05-24 15:50:43 -0300

    solving some problems

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 # Global var
 strPDF = ""
 textScanned = ""
+dirName = ["images", "output_txt"]
 
 # Get input from User
 def gInUs():
@@ -32,63 +33,89 @@ def gInUs():
 
 # Extracting images
 def extIm(fileStr):
-    dirName = "images"
+    global dirName
 
     # open the file
     pdf_file = fitz.open(fileStr)
 
     # Create output folder if don't exists
-    try:
-        os.makedirs(dirName)
-        print(Fore.GREEN + "[!] Directory " , dirName ,  " Created"+ Fore.RESET)
-    except FileExistsError:
-        print(Fore.RED + "[X] Directory " , dirName ,  " already exists" + Fore.RESET)
+    for i in dirName:
+        try:
+            os.makedirs(i)
+            print(Fore.GREEN + "[!] Directory " , i ,  " Created"+ Fore.RESET)
+        except FileExistsError:
+            print(Fore.RED + "[X] Directory " , i ,  " already exists" + Fore.RESET)
 
+    # List images if exists and print each one. if not extract all images uWu
+    content = os.listdir("images")
+    if(len(content) >= 1):
+        # Print every img in content
+        for i in content:
+            print(Fore.YELLOW + f"This is an image: {i}" + Fore.RESET)
+    else:
+        # Iterate over PDF pages
+        for page_index in range(len(pdf_file)):
 
-    # iterate over PDF pages
-    for page_index in range(len(pdf_file)):
+            # get the page itself
+            page = pdf_file[page_index]
+            image_list = page.getImageList()
 
-        # get the page itself
-        page = pdf_file[page_index]
-        image_list = page.getImageList()
+            # printing number of images found in this page
+            if image_list:
+                print(Fore.GREEN + f"[+] Found a total of {len(image_list)} images in page {page_index}" + Fore.RESET)
+            else:
+                print(Fore.RED + "[!] No images found on page", page_index, Fore.RESET)
 
-        # printing number of images found in this page
-        if image_list:
-            print(f"[+] Found a total of {len(image_list)} images in page {page_index}")
-        else:
-            print(Fore.RED + "[!] No images found on page", page_index, Fore.RESET)
-
-        for (image_index, img) in enumerate(page.getImageList(), start=1):
-            # get the XREF of the image
-            xref = img[0]
-            # extract the image bytes
-            base_image = pdf_file.extractImage(xref)
-            image_bytes = base_image["image"]
-            # get the image extension
-            image_ext = base_image["ext"]
-            # load it to PIL
-            image = Image.open(io.BytesIO(image_bytes))
-            # save it to local disk
-            image.save(open(f"images/image{page_index+1}_{image_index}.{image_ext}", "wb"))
+            for (image_index, img) in enumerate(page.getImageList(), start=1):
+                # get the XREF of the image
+                xref = img[0]
+                # extract the image bytes
+                base_image = pdf_file.extractImage(xref)
+                image_bytes = base_image["image"]
+                # get the image extension
+                image_ext = base_image["ext"]
+                # load it to PIL
+                image = Image.open(io.BytesIO(image_bytes))
+                # save it to local disk
+                image.save(open(f"images/image{page_index+1}_{image_index}.{image_ext}", "wb"))
     reImg()
 
 def reImg():
     global textScanned
 
-    # List the images
-    content = os.listdir('images')
     pytesseract.pytesseract.tesseract_cmd = r'R:/Programas\Tesseract\tesseract.exe'
 
+    
+    # List the images
+    content = os.listdir('images')
+
     for i in range(len(content)):
+        # Global var
+        global dirName
 
+        # Reading each image in images
         image = cv2.imread(f'images/{content[i]}')
+
+        # Scan text from image
+        print(Fore.YELLOW + f"[.] Scan text from {content[i]}" + Fore.RESET)
         text = pytesseract.image_to_string(image,lang='spa')
+
+        # Concate text scanned in a string
         textScanned += text
-        print('Texto: ',text)
 
+        # print
+        print(Fore.GREEN + "[!] Finished scan text" + Fore.RESET)
 
-        # cv2.imshow('Image',image)
-        # cv2.waitKey(10)
-        # cv2.destroyAllWindows()
+        # Create and write file txtResult.txt
+        print(Fore.CYAN + "[.] Writing txtResult.txt" + Fore.RESET)
+        fileTxt = open(f"{dirName[1]}/txtResult.txt", "w")
+        fileTxt.write(text)
+        print(Fore.GREEN + "[!] File Writted" + Fore.RESET)
+
+        # Showing img input
+        cv2.imshow('Image',image)
+        # 0.5 milisecond
+        cv2.waitKey(500)
+        cv2.destroyAllWindows()
 # Call to fun main
 gInUs()
